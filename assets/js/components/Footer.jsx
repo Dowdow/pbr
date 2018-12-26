3864ebe4e47209ba178fd3a65e09d778857046c6
Léo Riera <leo.riera1@gmail.com>
2018-12-26 23:12:45 +0100

    Change Instagram link

--- assets/js/components/Footer.jsx
+++ assets/js/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () =>
         <div className="divider"/>
         <h2>Social Networks</h2>
         <h3>
-            <a href="https://www.instagram.com/jlouriera" target="_blank" rel="noopener">Instagram</a>
+            <a href="https://www.instagram.com/painboudinrecord" target="_blank" rel="noopener">Instagram</a>
         </h3>
         <h3>
             <a href="https://soundcloud.com/painboudinrecord" target="_blank" rel="noopener">Soundcloud</a>
