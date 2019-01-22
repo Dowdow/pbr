502d19e2ab4f3aabdef15c6cf8ad4bb94d9c2a00
Léo Riera <leo.riera1@gmail.com>
2019-01-22 22:14:06 +0100

    Facebook + Mail + Sitemaps.xml + Robots.txt

--- assets/js/components/Footer.jsx
+++ assets/js/components/Footer.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 
 const Footer = () =>
     <footer>
-        <div className="divider"/>
-        <h2>Social Networks</h2>
-        <h3>
-            <a href="https://www.instagram.com/painboudinrecord" target="_blank" rel="noopener">Instagram</a>
-        </h3>
-        <h3>
-            <a href="https://soundcloud.com/painboudinrecord" target="_blank" rel="noopener">Soundcloud</a>
-        </h3>
-        <h3>
-            <a href="https://www.twitch.tv/painboudinrecord" target="_blank" rel="noopener">Twitch</a>
-        </h3>
+        <section>
+            <div className="divider"/>
+            <h2>Social Networks</h2>
+            <h3>
+                <a href="https://www.facebook.com/PainBoudinRecord/" target="_blank" rel="noopener">Facebook</a>
+            </h3>
+            <h3>
+                <a href="https://www.instagram.com/painboudinrecord" target="_blank" rel="noopener">Instagram</a>
+            </h3>
+            <h3>
+                <a href="https://soundcloud.com/painboudinrecord" target="_blank" rel="noopener">Soundcloud</a>
+            </h3>
+            <h3>
+                <a href="https://www.twitch.tv/painboudinrecord" target="_blank" rel="noopener">Twitch</a>
+            </h3>
+        </section>
+        <section>
+            <div className="divider"/>
+            <h2>Contact</h2>
+            <h3>
+                <a href="mailto:jlou@painboudinrecord.fr" title="jlou@painboudinrecord.fr">Mail</a>
+            </h3>
+        </section>
     </footer>
 ;
 
