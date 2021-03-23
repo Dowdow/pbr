b95253cfcfc576967fc8059df2dde905d4d1ee5f
Léo Riera <leo.riera1@gmail.com>
2021-03-23 17:36:02 +0100

    Hide some pages

--- assets/js/components/Footer.jsx
+++ assets/js/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faYoutube, faTwitch, faApple, faSoundcloud, faDeezer, faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faSoundcloud } from '@fortawesome/free-brands-svg-icons';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () =>
@@ -8,22 +8,11 @@ const Footer = () =>
       <section>
          <a href="https://www.instagram.com/pbdr_music/"
             target="_blank" rel="noopener"><FontAwesomeIcon icon={faInstagram} /> Instagram</a>
-         <a href="https://www.youtube.com/channel/UCA-XJa03nOVrOppElgW1jdw"
-            target="_blank" rel="noopener"><FontAwesomeIcon icon={faYoutube} /> YouTube</a>
-         <a href="https://www.twitch.tv/painboudinrecord"
-            target="_blank" rel="noopener"><FontAwesomeIcon icon={faTwitch} /> Twitch</a>
-         <a href="mailto:jlou@painboudinrecord.fr" title="jlou@painboudinrecord.fr"><FontAwesomeIcon icon={faPaperPlane} /> Mail</a>
-      </section>
-      <section>
-         <a href="https://itunes.apple.com/us/artist/pain-boudin-record/1458640395"
-            target="_blank" rel="noopener"><FontAwesomeIcon icon={faApple} /> Apple Music</a>
          <a href="https://soundcloud.com/pbdr_music"
             target="_blank" rel="noopener"><FontAwesomeIcon icon={faSoundcloud} /> Soundcloud</a>
-         <a href="https://www.deezer.com/fr/artist/62915352"
-            target="_blank" rel="noopener"><FontAwesomeIcon icon={faDeezer} /> Deezer</a>
-         <a href="https://open.spotify.com/artist/74FhxjKBLi7hYdTCm0r28w?si=vVbnBgVBTt6UJKYQviPpzQ"
-            target="_blank" rel="noopener"><FontAwesomeIcon icon={faSpotify} /> Spotify</a>
+         <a href="mailto:jlou@painboudinrecord.fr" title="jlou@painboudinrecord.fr"><FontAwesomeIcon icon={faPaperPlane} /> Mail</a>
       </section>
+      <section></section>
    </footer>
    ;
 
--- assets/js/components/Header.jsx
+++ assets/js/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
             <nav>
                 <NavLink to="/" exact={true} activeClassName="active">Home</NavLink>
                 <NavLink to="/songs" activeClassName="active">Songs</NavLink>
-                <NavLink to="/videos" activeClassName="active">Videos</NavLink>
-                <NavLink to="/live" activeClassName="active">Live</NavLink>
+                {/*<NavLink to="/videos" activeClassName="active">Videos</NavLink>*/}
+                {/*<NavLink to="/live" activeClassName="active">Live</NavLink>*/}
                 <a href={process.env.NODE_ENV === 'production' ? 'https://shop.painboudinrecord.fr' : 'http://shop.pbr.local'} target="_blank" rel="noopener">Shop</a>
                 {admin ? <a href="/admin">Admin</a> : ''}
             </nav>
